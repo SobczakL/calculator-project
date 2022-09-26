d8e941668a06556783e9bc298ffe8908b15f3f42
lucqaz01 <Lucassobczak3@gmail.com>
2022-09-26 16:48:43 -0400

    Pre-Final
    Logic not working

--- scirpt.js
+++ scirpt.js
@@ -5,14 +5,15 @@ class Calculator{
         this.currentOperandTextElement = currentOperandTextElement
         this.clear()
     }
-    clear(){
+
+    clear() {
         this.currentOperand = ''
-        this.previousOper = ''
+        this.previousOperand = ''
         this.operation = undefined
     }
     
     delete(){
-    
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
     
     appendNumber(number){
@@ -21,15 +22,66 @@ class Calculator{
     }
     
     chooseOperation(operation){
-    
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operationand
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
     
-    compute(){
-    
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+          case '+':
+            computation = prev + current
+            break
+          case '-':
+            computation = prev - current
+            break
+          case '*':
+            computation = prev * current
+            break
+          case '÷':
+            computation = prev / current
+            break
+          default:
+            return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
     }
     
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
+
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const intergerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let intergerDisplay
+        if (isNaN(intergerDigits)) {
+            intergerDisplay = ''
+        } else {
+            intergerDisplay = intergerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if(decimalDigits != null) {
+            return `${intergerDisplay}.${decimalDigits}`
+        } else {
+            return intergerDisplay
+        }
     }
 }
 
@@ -50,11 +102,26 @@ numberButtons.forEach(button => {
       calculator.appendNumber(button.innerText)
       calculator.updateDisplay()
     })
-  })
+})
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
       calculator.chooseOperation(button.innerText)
       calculator.updateDisplay()
     })
-  })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
